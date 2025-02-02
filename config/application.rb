79bd95fe105ad7d4b483a10eb7bcd722ed00f2b8
Marco Pereira <marcocsp@gmail.com>
2025-02-02 23:27:12 +0000

    Change some configurations related to annotate and application

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.eager_load_paths += %W[#{config.root}/app/errors]
+    config.autoload_paths += %W[#{config.root}/lib]
   end
 end
