7fc8781107f9e7c548a617105223429666dbff4b
Marco Pereira <marcocsp@gmail.com>
2025-02-02 23:30:44 +0000

    Ensure that BuyOrder has buyer_id instead user_id

--- db/migrate/20250128215051_create_buy_orders.rb
+++ db/migrate/20250128215051_create_buy_orders.rb
@@ -1,7 +1,7 @@
 class CreateBuyOrders < ActiveRecord::Migration[8.0]
   def change
     create_table :buy_orders do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :buyer, null: false, foreign_key: { to_table: :users }
       t.references :business, null: false, foreign_key: true
       t.integer :quantity, null: false
       t.decimal :price, null: false, precision: 10, scale: 2
