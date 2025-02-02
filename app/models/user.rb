4f2af5d7e4cd0f8046244ff2b5b6cf6f8c79169e
Marco Pereira <marcocsp@gmail.com>
2025-02-02 23:26:16 +0000

    Add security to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,17 @@
 #  id              :integer          not null, primary key
 #  username        :string(255)      not null
 #  password_digest :string(255)      not null
-#  role            :string(255)      not null
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
 
 class User < ApplicationRecord
-  has_many :owned_businesses, class_name: 'Business', foreign_key: "owner_id"
-  has_many :buy_orders
+  has_secure_password
+
+  has_many :owned_businesses, class_name: "Business", foreign_key: "owner_id"
+  has_many :buy_orders, class_name: "BuyOrder", foreign_key: "buyer_id"
   has_many :transactions
 
   validates :username, presence: true, uniqueness: true
   validates :password_digest, presence: true
-  validates :role, presence: true, inclusion: { in: %w[buyer owner] }
-
 end
