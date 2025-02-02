534c4bfdb9a280fe7bd337c49476f03117f5d51c
Marco Pereira <marcocsp@gmail.com>
2025-02-02 23:24:34 +0000

    Add helper methods to Business model: is_available?, pending_shares and remaining_shares

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,9 +1,23 @@
 class Business < ApplicationRecord
   has_many :buy_orders
   has_many :transactions
-  belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
+  belongs_to :owner, class_name: "User", foreign_key: "owner_id"
 
   validates :name, presence: true
   validates :total_shares, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :available_shares, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  scope :available, -> { where("available_shares > 0") }
+
+  def is_available?
+    available_shares > 0
+  end
+
+  def pending_shares
+    buy_orders.pending.sum(:quantity)
+  end
+
+  def remaining_shares
+    available_shares - pending_shares
+  end
 end
